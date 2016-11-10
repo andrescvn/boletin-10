54d8bf5b6c731c7f726f06ae9de2f68f39c21ec3
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-11-10 12:39:27 +0100

    boletin 9

---
+++ src/boletin/pkg10/Xogo.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg10;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Xogo {
+    public void xogo (){
+        float n;
+        int i;
+        float m;
+        int c=0;
+        do {
+            n=Float.parseFloat(JOptionPane.showInputDialog("numero"));
+        }while (n<1||n>50);
+        do {
+            i=Integer.parseInt(JOptionPane.showInputDialog("intentos"));
+        }while (i<=0);
+        do{
+            m=Float.parseFloat(JOptionPane.showInputDialog("numero"));
+            if (m<n){
+                System.out.println("maior");
+                c++;}
+            else if (m>n){
+                    System.out.println("menor");
+                    c++;
+            }
+            else
+                System.out.println("correcto");
+        }while (m!=n&&c!=i); 
+    }
+    public void xogo2(){
+        int c=0;
+        int n;
+        int i;
+          int e = (int) Math.floor(Math.random()*50+1);
+          System.out.println(e);
+        do {
+            i=Integer.parseInt(JOptionPane.showInputDialog("intentos"));
+        }while (i<=0);
+       do{
+           do {
+            n=Integer.parseInt(JOptionPane.showInputDialog("numero"));
+           } while (n<1||n>50);
+            int r=e-n;
+            if (r>20){
+            System.out.println("moi lonxe"); 
+            c++;}
+            else if (r>=10&&r<=20){
+            System.out.println("lonxe"); 
+            c++;}
+            else if (r<10&&r>5){
+            System.out.println("preto");
+            c++;}
+           else if (r<=5&&r!=0){   
+            System.out.println("moi preto");
+            c++;}
+            else if (r==0)
+            System.out.println("correcto");
+        }while (n!=e&&c!=i); 
+    }        
+}
